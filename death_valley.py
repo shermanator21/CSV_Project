1e8f85fe8467fc77ac3be4d16b3a91aba9743c83
Joseph Sherman <shermanator2018@gmail.com>
2021-02-22 14:14:01 -0600

    sitka 3 and death valley initial commits

--- death_valley.py
+++ death_valley.py
@@ -1,7 +1,7 @@
 import csv 
 from datetime import datetime
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter = ",")
 
@@ -12,16 +12,23 @@ header_row = next(csv_file)
 for index, column_header in enumerate(header_row):
     print("Index:", index, "Column Name:", column_header)
 
+
 highs = []
 dates = []
-
-
+lows = []
 
 
 for row in csv_file:
-    highs.append(int(row[5]))
-    converted_date = datetime.strptime(row[2], "%Y-%m-%d")
-    dates.append(converted_date)
+    try:
+        high = int(row[4])
+        low = int(row[5])
+        converted_date = datetime.strptime(row[2], "%Y-%m-%d")
+    except ValueError:
+        print(f"missing data for {converted_date}")
+    else: 
+        highs.append(high)
+        lows.append(low)
+        dates.append(converted_date)
 
 #plot highs on a chart
 
@@ -30,12 +37,16 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 
 plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
 
 fig.autofmt_xdate()
 
-plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
 
 plt.show()
+
--- sitka_3.py
+++ sitka_3.py
@@ -1,7 +1,14 @@
+# 1. change the file to include all the data for the year 2018
+# 2. change the title to - Daily and low high temperatures - 2018
+# 3. 
+# 4. 
+# 5. 
+
+
 import csv 
 from datetime import datetime
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("sitka_weather_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter = ",")
 
@@ -14,12 +21,12 @@ for index, column_header in enumerate(header_row):
 
 highs = []
 dates = []
-
-
+lows = []
 
 
 for row in csv_file:
     highs.append(int(row[5]))
+    lows.append(int(row[6]))
     converted_date = datetime.strptime(row[2], "%Y-%m-%d")
     dates.append(converted_date)
 
@@ -30,12 +37,22 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 
 plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
 
 fig.autofmt_xdate()
 
-plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
+
+plt.show()
+
+fig2, a=plt.subplots(2)
+
+a[0].plot(dates, highs, c="red")
+a[1].plot(dates, lows, c="blue")
 
 plt.show()
